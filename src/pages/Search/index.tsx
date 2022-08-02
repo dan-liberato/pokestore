976153169c60f01f06b5886ea19372e97ea06ae6
dan-liberato <danielrcliberato@gmail.com>
2022-08-01 23:29:35 -0300

    chore: intall react-router-dom, config Routes and change components to use arrow function

---
+++ src/pages/Search/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Search = () => {
+  return (
+    <div>
+      <h1>PokeStore | Search Results</h1>
+
+      <p>This is a paragraph.</p>
+    </div>
+  );
+};
+export default Search;

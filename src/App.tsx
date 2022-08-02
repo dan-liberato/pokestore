ff8f5eb0b397e451d525270a0b0dd1081fda953e
dan-liberato <danielrcliberato@gmail.com>
2022-08-01 23:05:52 -0300

    fix: lint-staged config

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <h1>Hello World</h1>
+
+      <p>This is a paragraph.</p>
     </div>
   );
 }
